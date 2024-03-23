b28afc08fcd5033b66da014ea295d5c5befe7601
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-03-23 16:18:46 +0900

    500 Status Error : WebConfig.java 추가

---
+++ src/main/java/an/rentalinhaee/WebConfig.java
@@ -0,0 +1,19 @@
+package an.rentalinhaee;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                //.allowCredentials(true) //주석처리함
+                .allowedOriginPatterns("*") //추가한 부분
+                .exposedHeaders("*");
+    }
+}
