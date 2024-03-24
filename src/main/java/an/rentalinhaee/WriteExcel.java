59d91c8a0adab4dfb5d13fffa5ba03bbbe6df0bc
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-03-25 00:48:24 +0900

    admin: 학생회비 납부 명단 추가 기능 추가

---
+++ src/main/java/an/rentalinhaee/WriteExcel.java
@@ -0,0 +1,38 @@
+package an.rentalinhaee;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+
+public class WriteExcel {
+    public static void writeToExcel(String stuId, String name) {
+        try {
+            // 파일을 읽기 위한 InputStream 생성
+            InputStream file = new FileInputStream("src/main/resources/static/studentId.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            // 새로운 행을 추가하기 위한 로직
+            int lastRowNum = sheet.getLastRowNum();
+            XSSFRow row = sheet.createRow(lastRowNum + 1);
+
+            // 셀에 데이터 쓰기
+            row.createCell(0).setCellValue(Integer.parseInt(stuId));
+            row.createCell(1).setCellValue(name);
+
+            // 변경 사항을 적용하기 위해 파일을 새로 쓰기
+            FileOutputStream fileOut = new FileOutputStream("src/main/resources/static/studentId.xlsx");
+            workbook.write(fileOut);
+
+            // 리소스 정리
+            fileOut.close();
+            workbook.close();
+            file.close(); // InputStream도 닫아주는 것이 좋습니다.
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/an/rentalinhaee/controller/StudentController.java
+++ src/main/java/an/rentalinhaee/controller/StudentController.java
@@ -1,5 +1,6 @@
 package an.rentalinhaee.controller;
 
+import an.rentalinhaee.WriteExcel;
 import an.rentalinhaee.domain.Rental;
 import an.rentalinhaee.domain.Student;
 import an.rentalinhaee.repository.RentalSearch;
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -68,25 +72,42 @@ public class StudentController {
     private final FeeStudentService feeStudentService;
 
     @GetMapping("/student/feeList")
-    String feeList(@ModelAttribute("studentSearch") StudentSearch studentSearch,
+    public String feeList(@ModelAttribute("studentSearch") StudentSearch studentSearch,
                 @RequestParam(required = false, defaultValue = "1", value = "page") int page,
-                Model model) {
+                Model model) throws IOException, InterruptedException {
 
         PageRequest pageRequest;
 
         // 학번, 이름 모두 포함하는 학생들 추가
         if (studentSearch.getStuId() != null && studentSearch.getName() != null) {
-            System.out.println("!!!!!!!!!! = ");
             pageRequest = PageRequest.of(page - 1, 10, Sort.by("stuId").descending());
             model.addAttribute("students", feeStudentService.findFeeStudent(studentSearch.getStuId(), studentSearch.getName(), pageRequest));
         } else {
             pageRequest = PageRequest.of(page - 1, 10, Sort.by("stuId").descending());
             model.addAttribute("students", feeStudentService.findFeeList(pageRequest));
+
         }
         model.addAttribute("studentSearch", studentSearch);
+
+
         return"student/feeList";
     }
 
+    @GetMapping("/student/feeList/add")
+    public String createFeeStudentForm(@RequestParam("newStuId") String stuId, @RequestParam("newName") String name, Model model) {
+
+        // writeExcel
+        if(!stuId.isEmpty() && !name.isEmpty()) {
+            WriteExcel.writeToExcel(stuId, name);
+
+        } else {    // 학번 or 이름 비어있는 경우
+            model.addAttribute("errorMessage", "학번과 이름에 공백이 있는지 확인해주세요!");
+            model.addAttribute("nextUrl", "/student/feeList");
+            return "error/errorMessage";
+        }
+
+        return "redirect:/student/feeList";
+    }
 
     @ModelAttribute("loginStuId")
     public String loginStuId(HttpSession httpSession) {
