3425e32cc402121e82cdbabe66af54bd2c552bf5
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-03-26 13:51:25 +0900

    RentalStatusUpdater 설정

---
+++ src/main/java/an/rentalinhaee/SpringConfig.java
@@ -0,0 +1,10 @@
+package an.rentalinhaee;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SpringConfig {
+
+}
