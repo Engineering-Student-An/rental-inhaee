361c331d266a038ef114444ed161952e4828620c
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-03-29 23:56:18 +0900

    Item 페이징 추가

--- src/main/java/an/rentalinhaee/controller/ItemController.java
+++ src/main/java/an/rentalinhaee/controller/ItemController.java
@@ -1,15 +1,16 @@
 package an.rentalinhaee.controller;
 
-import an.rentalinhaee.domain.Item;
+import an.rentalinhaee.repository.ItemSearch;
 import an.rentalinhaee.service.ItemService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,14 +19,21 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("/item/list")
-    public String list(Model model, HttpSession httpSession) {
-        List<Item> items = itemService.findAllItems();
-        model.addAttribute("items", items);
+    public String list(@ModelAttribute("itemSearch") ItemSearch itemSearch,
+                       @RequestParam(required = false, defaultValue = "1", value = "page") int page, Model model) {
 
+        PageRequest pageRequest;
 
-        String stuId = (String) httpSession.getAttribute("stuId");
-        model.addAttribute("stuId", stuId);
+        // 학번, 이름 모두 포함하는 학생들 추가
+        if (itemSearch.getCategory() != null && itemSearch.getName() != null) {
+            pageRequest = PageRequest.of(page - 1, 10, Sort.by("category").and(Sort.by("name")));
+            model.addAttribute("items", itemService.findItemsByCategoryAndName(itemSearch.getCategory(), itemSearch.getName(), pageRequest));
+        } else {
+            pageRequest = PageRequest.of(page - 1, 10, Sort.by("category").and(Sort.by("name")));
+            model.addAttribute("items", itemService.findAllItems(pageRequest));
+        }
 
+        model.addAttribute("itemSearch", itemSearch);
         return "item/list";
     }
 
