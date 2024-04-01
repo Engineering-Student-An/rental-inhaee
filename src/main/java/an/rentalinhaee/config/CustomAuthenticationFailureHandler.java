95fde5d82f11b3f8e9aa3ef51a4050084fce9181
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-04-01 14:53:44 +0900

    로그인 실패 url 추가

---
+++ src/main/java/an/rentalinhaee/config/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,18 @@
+package an.rentalinhaee.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        response.sendRedirect("/login/error"); // 로그인 페이지로 리다이렉트
+    }
+}
