9d6d5fe69bdcb22ae2c526d5af0887618ea68cc1
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-04-09 23:37:01 +0900

    mobile: 화면 조정

--- src/main/java/an/rentalinhaee/controller/admin/AdminRentalController.java
+++ src/main/java/an/rentalinhaee/controller/admin/AdminRentalController.java
@@ -3,9 +3,7 @@ package an.rentalinhaee.controller.admin;
 import an.rentalinhaee.domain.Rental;
 import an.rentalinhaee.domain.Student;
 import an.rentalinhaee.repository.RentalSearch;
-import an.rentalinhaee.service.ItemService;
 import an.rentalinhaee.service.RentalService;
-import an.rentalinhaee.service.StudentService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -22,55 +20,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class AdminRentalController {
 
     private final RentalService rentalService;
-    private final StudentService studentService;
-    private final ItemService itemService;
-
-
-
-
-//    @GetMapping("/rental/findOne")
-//    public String rentalList(@RequestParam(required = false, defaultValue = "1", value = "page") int page,
-//                             @ModelAttribute("rentalSearch") RentalSearch rentalSearch,
-//                             Model model) {
-//
-//        Student student = (Student) model.getAttribute("loginStudent");
-//
-//        rentalSearch.setStuId(student.getStuId());
-//
-//        PageRequest pageRequest = PageRequest.of(page-1, 10, Sort.by("status").and(Sort.by("rentalDate")));
-//        Page<Rental> rentalList;
-//        if(rentalSearch.getRentalStatus() == null){
-//            rentalList = rentalService.findMyRentalList(student.getId(), pageRequest);
-//        } else{
-//            rentalList = rentalService.findRentals(rentalSearch, pageRequest);
-//        }
-//
-//        model.addAttribute("myRentalList", rentalList);
-//
-//        return "rental/findOne";
-//    }
-//
-//    @PostMapping("/rental/findOne/{rentalId}/finish")
-//    public String finishRental(@PathVariable("rentalId") Long rentalId, Model model) {
-//        if(ChronoUnit.DAYS.between(rentalService.findRentalByRentalId(rentalId).getRentalDate(), LocalDate.now()) > 3){
-//            model.addAttribute("errorMessage", "연체중인 물품의 반납은 관리자를 통해 가능합니다.");
-//            model.addAttribute("nextUrl", "/rental/findOne");
-//            model.addAttribute("newUrl", "https://pf.kakao.com/_CxjxlxiM");
-//            return "error/errorMessageNewTab";
-//        }
-//
-//        model.addAttribute("errorMessage", "해당 물품을 반납 처리 하시겠습니까?\n반납이 이루어지지 않았을 경우, 불이익이 주어질 수 있습니다!");
-//
-//        model.addAttribute("yesUrl","/rental/findOne/" + rentalId + "/finish/confirm");
-//        model.addAttribute("noUrl","/rental/findOne");
-//        return "error/yesOrNoMessage";
-//    }
-//
-//    @GetMapping("/rental/findOne/{rentalId}/finish/confirm")
-//    public String finishRentalConfirm(@PathVariable("rentalId") Long rentalId) {
-//        rentalService.finishRental(rentalId);
-//        return "redirect:/rental/findOne";
-//    }
 
     @PostMapping("/rental/{rentalId}/finish")
     public String adminFinishRental(@PathVariable("rentalId") Long rentalId, @RequestParam String stuId, RedirectAttributes redirectAttributes) {
@@ -92,6 +41,9 @@ public class AdminRentalController {
         Page<Rental> rentalByStuIdStatusItem = rentalService.findRentalByStuId_Status_Item(rentalSearch, pageRequest);
         model.addAttribute("rentals", rentalByStuIdStatusItem);
 
+        if ((boolean) model.getAttribute("isMobile")) {
+            return "mobile/admin/rental/list";
+        }
         return "admin/rental/list";
     }
 
