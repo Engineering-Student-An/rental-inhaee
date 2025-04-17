af7a8a2c39027ce972389501361377a4c9450ee6
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-04-17 23:38:58 +0900

    remove: BoardQueryRepository 삭제

--- src/main/java/an/rentalinhaee/repository/BoardQueryRepository.java
+++ src/main/java/an/rentalinhaee/repository/BoardQueryRepository.java
@@ -1,56 +1,56 @@
-package an.rentalinhaee.repository;
-
-import an.rentalinhaee.domain.Board;
-import an.rentalinhaee.domain.QBoard;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
-public class BoardQueryRepository {
-
-    private final EntityManager em;
-
-
-    // 최근 게시글 쿼리
-    public List<Board> findRecentBoard() {
-        JPAQueryFactory query = new JPAQueryFactory(em);
-        QBoard board = QBoard.board;
-
-        long totalBoardCount = query.selectFrom(board).fetchCount();
-
-        if(totalBoardCount == 0) {
-            totalBoardCount = 1;
-        }
-        long limit = totalBoardCount < 5 ? totalBoardCount : 5;
-
-        return query.selectFrom(board)
-                .orderBy(board.writeTime.desc())
-                .limit(limit).fetch();
-    }
-
-    // hot 게시글 쿼리
-    public List<Board> findHotBoard() {
-        JPAQueryFactory query = new JPAQueryFactory(em);
-        QBoard board = QBoard.board;
-
-        long totalBoardCount = query.selectFrom(board).fetchCount();
-
-        if(totalBoardCount == 0) {
-            totalBoardCount = 1;
-        }
-        long limit = totalBoardCount < 5 ? totalBoardCount : 5;
-
-        return query.selectFrom(board)
-                .orderBy(board.likeNumber.size().desc(), board.writeTime.desc())
-                .where(board.writeTime.between(LocalDateTime.now().minusDays(7), LocalDateTime.now()))
-                .where(board.likeNumber.isNotEmpty())
-                .limit(limit).fetch();
-    }
-
-}
+//package an.rentalinhaee.repository;
+//
+//import an.rentalinhaee.domain.Board;
+//import an.rentalinhaee.domain.QBoard;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import jakarta.persistence.EntityManager;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Repository;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//@Repository
+//@RequiredArgsConstructor
+//public class BoardQueryRepository {
+//
+//    private final EntityManager em;
+//
+//
+//    // 최근 게시글 쿼리
+//    public List<Board> findRecentBoard() {
+//        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QBoard board = QBoard.board;
+//
+//        long totalBoardCount = query.selectFrom(board).fetchCount();
+//
+//        if(totalBoardCount == 0) {
+//            totalBoardCount = 1;
+//        }
+//        long limit = totalBoardCount < 5 ? totalBoardCount : 5;
+//
+//        return query.selectFrom(board)
+//                .orderBy(board.writeTime.desc())
+//                .limit(limit).fetch();
+//    }
+//
+//    // hot 게시글 쿼리
+//    public List<Board> findHotBoard() {
+//        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QBoard board = QBoard.board;
+//
+//        long totalBoardCount = query.selectFrom(board).fetchCount();
+//
+//        if(totalBoardCount == 0) {
+//            totalBoardCount = 1;
+//        }
+//        long limit = totalBoardCount < 5 ? totalBoardCount : 5;
+//
+//        return query.selectFrom(board)
+//                .orderBy(board.likeNumber.size().desc(), board.writeTime.desc())
+//                .where(board.writeTime.between(LocalDateTime.now().minusDays(7), LocalDateTime.now()))
+//                .where(board.likeNumber.isNotEmpty())
+//                .limit(limit).fetch();
+//    }
+//
+//}
